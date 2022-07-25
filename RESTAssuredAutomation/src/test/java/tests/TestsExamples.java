a0e1a16937b4f1ff8981a0b6d851ad8feb8c1ae9
sanskrutiginoya1 <sanskruti.ginoya1@gmail.com>
2022-07-25 13:12:22 -0400

    first commit

---
+++ RESTAssuredAutomation/src/test/java/tests/TestsExamples.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class TestsExamples {
+	
+	@Test
+	public void test_1() {
+		
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getTime());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-tpype"));
+		
+		
+	}
+
+}
